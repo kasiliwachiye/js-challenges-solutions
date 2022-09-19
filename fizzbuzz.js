723100d23b143fcd9042062f676e6faadb2c51bb
kasiliwachiye <63935096+kasiliwachiye@users.noreply.github.com>
2022-09-19 14:14:17 +0300

    add another fizzbuzz solution

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,14 +1,29 @@
-// how to solve fizzbuzz in javascript?
+function fizzBuzz() {
+  for (let i = 1; i < 101; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("fizzbuzz");
+    } else if (i % 3 === 0) {
+      console.log("fizz");
+    } else if (i % 5 === 0) {
+      console.log("buzz");
+    } else {
+      console.log(i);
+    }
+  }
+}
+
+console.log(fizzBuzz());
+
+// how to write javascript fizzbuzz function?
 /*Write a program that prints the numbers from 1 to 100. But for multiples of three print “Fizz” instead of the number and for the multiples of five print “Buzz”. For numbers which are multiples of both three and five print “FizzBuzz”*/
 
 var str = "",
   x,
   y,
-  num;
-
-for (num = 1; num <= 100; num++) {
-  x = num % 3 == 0;
-  y = num % 5 == 0;
+  a;
+for (a = 1; a <= 100; a++) {
+  x = a % 3 == 0;
+  y = a % 5 == 0;
   if (x) {
     str += "fizz";
   }
@@ -16,10 +31,8 @@ for (num = 1; num <= 100; num++) {
     str += "buzz";
   }
   if (!(x || y)) {
-    str += num;
+    str += a;
   }
-  // str += "\n";
-  str += ", ";
+  str += "\n";
 }
-
 console.log(str);
